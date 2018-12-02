dc61dcc123106cef22b83bbad1a649ca0f41daac
amirova160395 <amirova_gulnara@mail.ru>
2018-12-02 21:37:24 +0500

    1) здесь объединены все 3 задания категории easy 2) Задача 1 не работает при отрицательных числах (пока не придумала, как этого избежать)

---
+++ lesson1-easy.py
@@ -0,0 +1,27 @@
+print ("Zadacha 1")
+n = int(input('Vvedite celoe chislo: '))
+while n != 0:
+    a = n % 10
+    print (a)
+    n = n // 10
+print ('Programma zavershena')
+
+
+print ("Zadacha 2")
+a = input (' Vvedite chislo a: ')
+b = input (' vvedite chislo b: ')
+c = a
+print (" a = ", b)
+print (" b = ", c)
+print ('Programma zavershena')
+
+
+
+print ("Zadacha 3")
+a = int(input(' Vash vozrast: '))
+if a >= 18:
+    print ("Dostup razreshen")
+else:
+    print ("Dostup zapreshen")
+print ('Programma zavershena')
+    
